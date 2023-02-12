561679a531ef8f0e6b470719a2985ce6fb28ce2d
llblch <mustqwert@gmail.com>
2023-02-12 03:37:24 -0800

    AirBnB_clone_models

--- models/city.py
+++ models/city.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel, Base
-from models import storage_type
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+"""This module creates a User class"""
 
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship('Place', backref='cities',
-                cascade='all, delete, delete-orphan')
-    else:
-        name = ''
-        state_id = ''
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Class for managing city objects"""
+
+    state_id = ""
+    name = ""
